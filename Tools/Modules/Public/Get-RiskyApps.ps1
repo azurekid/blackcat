37ab2b1178150a80ae2ea12d9df21080d4be82f6
Rogier Dijkman <40334679+azurekid@users.noreply.github.com>
2024-11-13 13:15:45 +0000

    refactor: Simplify Get-RiskyApps function by optimizing variable assignments and improving readability

--- Tools/Modules/Public/Get-RiskyApps.ps1
+++ Tools/Modules/Public/Get-RiskyApps.ps1
@@ -1,82 +1,67 @@
 function Get-RiskyApps {
-
     begin {
         $MyInvocation.MyCommand.Name | Invoke-BlackCat -ResourceTypeName "MSGraph"
     }
 
     process {
-
         try {
-
             Write-Message -FunctionName $MyInvocation.MyCommand.Name -Message "Collecting Enterprise Applications"
-            $applications = (Invoke-GraphRecursive -Url "$($sessionVariables.graphUri)/applications")
+            $applications = Invoke-GraphRecursive -Url "$($sessionVariables.graphUri)/applications"
 
             Write-Verbose "User Applications: $($applications.count)"
             Write-Verbose "      [-] Validating [$($applications.count)] Enterprise Applications"
 
-            $permissionList = (Invoke-WebRequest 'https://raw.githubusercontent.com/SecureHats/SecureHacks/main/documentation/AppRegistrationPermissions.csv').content | ConvertFrom-Csv
-            $riskyGrants = $permissionList | Where-Object Permission -in `
-            (`
-                    'Directory.ReadWrite.All', `
-                    'PrivilegedAccess.ReadWrite.AzureAD', `
-                    'PrivilegedAccess.ReadWrite.AzureADGroup', `
-                    'PrivilegedAccess.ReadWrite.AzureResources', `
-                    'Policy.ReadWrite.ConditionalAccess', `
-                    'GroupMember.ReadWrite.All' `
+            $permissionList = (Invoke-WebRequest 'https://raw.githubusercontent.com/SecureHats/SecureHacks/main/documentation/AppRegistrationPermissions.csv').Content | ConvertFrom-Csv
+            $riskyGrants = $permissionList | Where-Object Permission -in @(
+                'Directory.ReadWrite.All',
+                'PrivilegedAccess.ReadWrite.AzureAD',
+                'PrivilegedAccess.ReadWrite.AzureADGroup',
+                'PrivilegedAccess.ReadWrite.AzureResources',
+                'Policy.ReadWrite.ConditionalAccess',
+                'GroupMember.ReadWrite.All'
             )
 
             $dataHash = New-Object System.Collections.ArrayList
             $riskyApps = New-Object System.Collections.ArrayList
-            $permissionObjects = @()
 
             foreach ($application in $applications) {
-                foreach ($riskyGrant in $riskyGrants) {
-                    if ($application.requiredResourceAccess.resourceaccess.id -contains $riskyGrant.id) {
-                        $permissionObjects += ($riskyGrant.Permission)
-                    }
+                $permissionObjects = @()
 
-                    if ($permissionObjects.count -gt 0) {
-                        $null = $riskyApps.add($application)
+                foreach ($riskyGrant in $riskyGrants) {
+                    if ($application.requiredResourceAccess.resourceAccess.id -contains $riskyGrant.id) {
+                        $permissionObjects += $riskyGrant.Permission
                     }
                 }
 
-                if ($permissionObjects.count -gt 0) {
+                if ($permissionObjects.Count -gt 0) {
+                    $null = $riskyApps.Add($application)
 
                     $currentItem = [PSCustomObject]@{
                         Id              = $application.Id
-                        DisplayName     = $application.displayname
-                        createdDateTime = $application.createdDateTime
+                        DisplayName     = $application.DisplayName
+                        CreatedDateTime = $application.CreatedDateTime
                         Permission      = $permissionObjects | Sort-Object -Unique
                     }
 
-                    if ($application.passwordCredentials.keyId) {
-                        $currentItem | Add-Member `
-                            -MemberType NoteProperty `
-                            -Name Credentials `
-                            -Value $application.passwordCredentials `
-                            -Force
+                    if ($application.PasswordCredentials.KeyId) {
+                        $currentItem | Add-Member -MemberType NoteProperty -Name Credentials -Value $application.PasswordCredentials -Force
                     }
 
-                    if ($application.keyCredentials.value) {
-                        $currentItem | Add-Member `
-                            -MemberType NoteProperty `
-                            -Name keyCredentials `
-                            -Value $application.keyCredentials `
-                            -Force
+                    if ($application.KeyCredentials.Value) {
+                        $currentItem | Add-Member -MemberType NoteProperty -Name KeyCredentials -Value $application.KeyCredentials -Force
                     }
 
                     [void]$dataHash.Add($currentItem)
                 }
-                $permissionObjects = @()
             }
 
             $json = [ordered]@{}
-            [void]$json.add("data", ($dataHash))
+            [void]$json.Add("data", $dataHash)
 
-            return $json.values
+            return $json.Values
         }
         catch {
-            Write-Message -FunctionName $($MyInvocation.MyCommand.Name) -Message $($_.Exception.Message) -Severity 'Error'
+            Write-Message -FunctionName $MyInvocation.MyCommand.Name -Message $_.Exception.Message -Severity 'Error'
         }
     }
 }
