12fc09a29beff0ee9db4aeac39004172ae1729ae
Rogier Dijkman (AzureKid) <rogierdijkman@hotmail.com>
2025-01-06 15:57:57 +0100

    Refactor Export-AccessToken function to use ResourceTypeNames instead of audiences for token requests

--- src/Public/generic/Export-AccessToken.ps1
+++ src/Public/generic/Export-AccessToken.ps1
@@ -2,7 +2,7 @@ function Export-AccessToken {
     [cmdletbinding()]
     param (
         [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
-        [array]$audiences = @("https://graph.microsoft.com/.default", "https://management.azure.com/.default", "https://vault.azure.net/.default"),
+        [array]$ResourceTypeNames = @("MSGraph", "ResourceManager", "KeyVault", "Storage", "Synapse", "OperationalInsights", "Batch"),
 
         [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
         [ValidateNotNullOrEmpty()]
@@ -18,47 +18,62 @@ function Export-AccessToken {
             Write-Verbose "Requesting access tokens for specified audiences"
             $tokens = @()
 
-            foreach ($audience in $audiences) {
-                $accessToken = Get-AzAccessToken -Audience $audience
-                $tokenObject = [PSCustomObject]@{
-                    Audience = $audience
-                    Token    = $accessToken
+            foreach ($resourceTypeName in $ResourceTypeNames) {
+                try {
+                    $accessToken = Get-AzAccessToken -ResourceTypeName $resourceTypeName
+                    $tokenContent = ConvertFrom-JWT -JWT $accessToken
+
+                    $tokenObject = [PSCustomObject]@{
+                        Resource = $resourceTypeName
+                        UPN      = $tokenContent.UPN
+                        Audience = $tokenContent.Audience
+                        Roles    = $tokenContent.Roles
+                        Tenant   = $tokenContent.Tenant
+                        Token    = $accessToken
+                    }
+                    $tokens += $tokenObject
                 }
-                $tokens += $tokenObject
+                catch {
+                    Write-Error "Failed to get access token for resource type $resourceTypeName : $($_.Exception.Message)"
+                }
             }
 
             Write-Verbose "Exporting tokens to file $OutputFile"
             $tokens | Out-File -FilePath $OutputFile
         }
         catch {
-            Write-Message -FunctionName $($MyInvocation.MyCommand.Name) -Message $($_.Exception.Message) -Severity 'Error'
+            Write-Error "An error occurred in function $($MyInvocation.MyCommand.Name): $($_.Exception.Message)"
         }
+    }
+
+    end {
+        Write-Verbose "Function $($MyInvocation.MyCommand.Name) completed"
     }
 
     <#
     .SYNOPSIS
-        This function exports access tokens for specified audiences to a text file.
+        This function exports access tokens for specified resource types to a text file.
 
     .DESCRIPTION
-        The Export-AccessToken function requests access tokens for the specified audiences and exports them to a text file. It handles errors and logs messages accordingly.
+        The Export-AccessToken function requests access tokens for the specified resource types and exports them to a text file. It handles errors and logs messages accordingly.
 
-    .PARAMETER audiences
-        The audiences parameter is an optional array of strings that specifies the audiences for which to request access tokens.
+    .PARAMETER ResourceTypeNames
+        The ResourceTypeNames parameter is an optional array of strings that specifies the resource types for which to request access tokens. Default values are "MSGraph", "ResourceManager", "KeyVault", "Storage", "Synapse", "OperationalInsights", and "Batch".
 
     .PARAMETER OutputFile
-        The OutputFile parameter is an optional string that specifies the path to the file where the tokens will be exported.
+        The OutputFile parameter is an optional string that specifies the path to the file where the tokens will be exported. The default value is "AccessTokens.txt".
 
     .EXAMPLE
         ```powershell
-        Export-AccessToken -audiences @("https://graph.microsoft.com/.default") -OutputFile "AccessTokens.txt"
+        Export-AccessToken -ResourceTypeNames @("MSGraph", "ResourceManager") -OutputFile "AccessTokens.txt"
         ```
-        This example calls the Export-AccessToken function with a specified audience and output file.
+        This example calls the Export-AccessToken function with specified resource types and output file.
 
     .EXAMPLE
         ```powershell
         Export-AccessToken -OutputFile "AccessTokens.txt"
         ```
-        This example calls the Export-AccessToken function with the default audiences and a specified output file.
+        This example calls the Export-AccessToken function with the default resource types and a specified output file.
 
     .LINK
         For more information, see the related documentation or contact support.
