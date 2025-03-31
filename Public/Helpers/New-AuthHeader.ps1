d79babc3b3ac491f76d370e6d4b0a2d49b870495
Rogier Dijkman <rogierdijkman@hotmail.com>
2025-03-31 18:11:54 +0200

    Enhance New-AuthHeader function: add support for custom endpoint URI and expand endpoint options

--- Public/Helpers/New-AuthHeader.ps1
+++ Public/Helpers/New-AuthHeader.ps1
@@ -2,19 +2,48 @@ function New-AuthHeader {
     [cmdletbinding()]
     param (
         [Parameter(Mandatory = $true)]
-        [ValidateSet('MSGraph', 'KeyVault', 'Azure', 'LogAnalytics')]
-        [string]$EndpointType
+        [ValidateSet('MSGraph', 'KeyVault', 'Azure', 'LogAnalytics', 'Other')]
+        [string]$EndpointType,
+
+        
+        [Parameter(Mandatory = $false)]
+        [ValidatePattern('^(https?)://[^\s/$.?#].[^\s]*$')]
+        [string]$endpointUri
     )
 
     begin {
         Write-Verbose "Starting function $($MyInvocation.MyCommand.Name)"
 
-        # Define endpoint resource URLs
-        $endpoints = @{
-            MSGraph      = 'https://graph.microsoft.com'
-            KeyVault     = 'https://vault.azure.net'
-            Azure        = 'https://management.azure.com'
-            LogAnalytics = 'https://api.loganalytics.io'
+        if ($EndpointType -eq 'Other') {
+            if ([string]::IsNullOrWhiteSpace($endpointUri)) {
+                Write-Message -FunctionName $($MyInvocation.MyCommand.Name) -Message "When 'Other' is selected as EndpointType, 'endpointUri' cannot be empty." -Severity 'Error'
+            }
+
+            # Use the provided endpoint URI
+            $endpoints = @{
+            Other = $endpointUri
+            }
+        } else {
+            # Use predefined endpoints for other types
+            $endpoints = @{
+                Azure        = 'https://management.azure.com'
+                Batch        = 'https://batch.azure.com'
+                Cache        = 'https://cache.azure.com'
+                CosmosDB     = 'https://cosmos.azure.com'
+                DataLake     = 'https://datalake.azure.net'
+                DevOps       = '499b84ac-1321-427f-aa17-267ca6975798'
+                EventGrid    = 'https://eventgrid.azure.net'
+                EventHub     = 'https://eventhub.azure.net'
+                IoTHub       = 'https://iothub.azure.net'
+                KeyVault     = 'https://vault.azure.net'
+                LogAnalytics = 'https://api.loganalytics.io'
+                MSGraph      = 'https://graph.microsoft.com'
+                RedisCache   = 'https://cache.azure.com'
+                SQLDatabase  = 'https://database.windows.net'
+                ServiceBus   = 'https://servicebus.azure.net'
+                Storage      = 'https://storage.azure.com'
+                Synapse      = 'https://dev.azuresynapse.net'
+            }
         }
     }
 
@@ -23,7 +52,7 @@ function New-AuthHeader {
             # Get the access token for the specified endpoint
             $context = Get-AzContext
             if (-not $context) {
-                throw "No Azure context found. Please run Connect-AzAccount first."
+                Write-Message -FunctionName $($MyInvocation.MyCommand.Name) -Message "No Azure context found. Please run Connect-AzAccount first." -Severity 'Error'
             }
 
             $token = Get-AzAccessToken -ResourceUrl $endpoints[$EndpointType]
