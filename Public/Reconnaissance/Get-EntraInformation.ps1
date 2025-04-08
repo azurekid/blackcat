4ced4aa4d9e6eba452c196c1e7d8b5032f011246
azurekid <rogierdijkman@hotmail.com>
2025-04-08 20:59:49 +0200

    Enhance parameter attributes in Switch-Context function and update user agent selection in Get-PublicStorageAccountList function

--- Public/Reconnaissance/Get-EntraInformation.ps1
+++ Public/Reconnaissance/Get-EntraInformation.ps1
@@ -19,31 +19,23 @@ function Get-EntraInformation {
 
     process {
         try {
-            # Base Graph API URL
-            $graphApiVersion = "beta"
-            $baseUri = "https://graph.microsoft.com/$graphApiVersion"
-
             # Construct query based on parameter set
             switch ($PSCmdlet.ParameterSetName) {
                 'ObjectId' {
                     if ($Group) {
-                        $uri = "$baseUri/groups/$ObjectId"
-                        $response = Invoke-RestMethod -Uri $uri -Method GET -Headers $script:graphHeader
+                        $response = Invoke-MsGraph -relativeUrl "groups/$ObjectId" -NoBatch
                         $isGroup = $true
                     } else {
-                        $uri = "$baseUri/users/$ObjectId"
-                        $response = Invoke-RestMethod -Uri $uri -Method GET -Headers $script:graphHeader
+                        $response = Invoke-MsGraph -relativeUrl "users/$ObjectId" -NoBatch
                         $isGroup = $false
                     }
                 }
                 'Name' {
                     if ($Group) {
-                        $uri = "$baseUri/groups?`$filter=startswith(displayName,'$Name')"
-                        $response = (Invoke-RestMethod -Uri $uri -Method GET -Headers $script:graphHeader).value
+                        $response = Invoke-MsGraph -relativeUrl "groups?`$filter=startswith(displayName,'$Name')"
                         $isGroup = $true
                     } else {
-                        $uri = "$baseUri/users?`$filter=startswith(displayName,'$Name') or startswith(userPrincipalName,'$Name')"
-                        $response = (Invoke-RestMethod -Uri $uri -Method GET -Headers $script:graphHeader).value
+                        $response = Invoke-MsGraph -relativeUrl "users?`$filter=startswith(displayName,'$Name') or startswith(userPrincipalName,'$Name')"
                         $isGroup = $false
                     }
                 }
@@ -52,12 +44,10 @@ function Get-EntraInformation {
             foreach ($item in $response) {
                 if ($isGroup) {
                     # Get group members
-                    $membersUri = "$baseUri/groups/$($item.id)/members"
-                    $members = (Invoke-RestMethod -Uri $membersUri -Method GET -Headers $script:graphHeader).value
+                    $members = Invoke-MsGraph -relativeUrl "groups/$($item.id)/members"
 
                     # Get group roles and permissions
-                    $rolesUri = "$baseUri/groups/$($item.id)/transitiveMemberOf/microsoft.graph.directoryRole"
-                    $roles = (Invoke-RestMethod -Uri $rolesUri -Method GET -Headers $script:graphHeader).value
+                    $roles = Invoke-MsGraph -relativeUrl "groups/$($item.id)/transitiveMemberOf/microsoft.graph.directoryRole"
 
                     # Create custom object with group information
                     [PSCustomObject]@{
@@ -73,23 +63,21 @@ function Get-EntraInformation {
                 } else {
                     # Rest of the code for users remains the same
                     # Get group memberships
-                    $groupsUri = "$baseUri/users/$($item.id)/memberOf"
-                    $groups = (Invoke-RestMethod -Uri $groupsUri -Method GET -Headers $script:graphHeader).value
+                    $groups = Invoke-MsGraph -relativeUrl "users/$($item.id)/memberOf"
 
                     # Get directory roles
-                    $rolesUri = "$baseUri/users/$($item.id)/transitiveMemberOf/microsoft.graph.directoryRole"
-                    $roles = (Invoke-RestMethod -Uri $rolesUri -Method GET -Headers $script:graphHeader).value
+                    $roles = Invoke-MsGraph -relativeUrl "users/$($item.id)/transitiveMemberOf/microsoft.graph.directoryRole"
 
                     # Create custom object with user information
                     [PSCustomObject]@{
                         UserPrincipalName = $item.userPrincipalName
-                        DisplayName      = $item.displayName
-                        ObjectId        = $item.id
-                        GroupMemberships = $groups.displayName
-                        Roles           = $roles.displayName
-                        Mail            = $item.mail
-                        JobTitle        = $item.jobTitle
-                        Department      = $item.department
+                        DisplayName       = $item.displayName
+                        ObjectId          = $item.id
+                        GroupMemberships  = $groups.displayName
+                        Roles             = $roles.displayName
+                        Mail              = $item.mail
+                        JobTitle          = $item.jobTitle
+                        Department        = $item.department
                     }
                 }
             }
