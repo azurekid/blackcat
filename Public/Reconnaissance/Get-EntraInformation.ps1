43c8e13d673436044cd9cf1c90de89bec66651f7
azurekid <rogierdijkman@hotmail.com>
2025-04-08 21:01:08 +0200

    Add detailed documentation for Get-EntraInformation function

--- Public/Reconnaissance/Get-EntraInformation.ps1
+++ Public/Reconnaissance/Get-EntraInformation.ps1
@@ -86,4 +86,42 @@ function Get-EntraInformation {
             Write-Message -FunctionName $($MyInvocation.MyCommand.Name) -Message $($_.Exception.Message) -Severity 'Error'
         }
     }
+<#
+.SYNOPSIS
+Retrieves information about Azure AD users or groups using Microsoft Graph API.
+
+.DESCRIPTION
+The Get-EntraInformation function queries Microsoft Graph API to retrieve detailed information about Azure AD users or groups.
+It supports querying by ObjectId or Name and can return additional details such as group memberships, roles, and permissions.
+
+.PARAMETER ObjectId
+Specifies the ObjectId of the user or group to retrieve information for. This parameter is mandatory when using the 'ObjectId' parameter set.
+
+.PARAMETER Name
+Specifies the display name or userPrincipalName of the user or group to retrieve information for. This parameter is mandatory when using the 'Name' parameter set.
+
+.PARAMETER Group
+Indicates that the query is for a group. If not specified, the query is assumed to be for a user.
+
+.EXAMPLE
+Get-EntraInformation -ObjectId "12345-abcde-67890" -Group
+Retrieves information about the group with the specified ObjectId.
+
+.EXAMPLE
+Get-EntraInformation -Name "John Doe"
+Retrieves information about the user with the specified display name or userPrincipalName.
+
+.EXAMPLE
+Get-EntraInformation -Name "Marketing" -Group
+Retrieves information about groups with display names starting with "Marketing".
+
+.NOTES
+- This function requires the Invoke-MsGraph cmdlet to interact with Microsoft Graph API.
+- Ensure that the required permissions are granted to the application or user executing this function.
+
+.OUTPUTS
+[PSCustomObject]
+Returns a custom object containing detailed information about the user or group, including roles, memberships, and other attributes.
+
+#>
 }
