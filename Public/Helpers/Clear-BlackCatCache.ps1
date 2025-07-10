660d559bc96c21184ee052dd8e603ab5d9ffe742
azurekid <rogierdijkman@hotmail.com>
2025-07-11 00:13:30 +0200

    refactor: Simplify Clear-BlackCatCache function by removing unnecessary comments and enhancing autocompletion for cache keys

--- Public/Helpers/Clear-BlackCatCache.ps1
+++ Public/Helpers/Clear-BlackCatCache.ps1
@@ -1,4 +1,64 @@
 function Clear-BlackCatCache {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)]
+        [ArgumentCompleter({
+            param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+            
+            $cacheType = $fakeBoundParameters['CacheType']
+            if (-not $cacheType) {
+                $cacheType = 'All'
+            }
+            
+            try {
+                $availableKeys = @()
+                $cacheTypes = if ($cacheType -eq 'All') { @('MSGraph', 'AzBatch', 'General') } else { @($cacheType) }
+                
+                foreach ($type in $cacheTypes) {
+                    try {
+                        $stats = Get-BlackCatCacheStats -CacheType $type -ShowDetails -Quiet
+                        
+                        if ($stats -and $stats.Entries -and $stats.Entries.Count -gt 0) {
+                            $availableKeys += $stats.Entries | ForEach-Object { $_.Key }
+                        }
+                    }
+                    catch {
+                        continue
+                    }
+                }
+                
+                $filteredKeys = $availableKeys | Where-Object { $_ -like "$wordToComplete*" } | Sort-Object
+                
+                $filteredKeys | ForEach-Object {
+                    if ($_ -match '[\s\|]') {
+                        "'$_'"
+                    } else {
+                        $_
+                    }
+                }
+            }
+            catch {
+                @()
+            }
+        })]
+        [string]$Key,
+        
+        [Parameter(Mandatory = $false)]
+        [switch]$All,
+
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('MSGraph', 'AzBatch', 'All')]
+        [string]$CacheType = 'All'
+    )
+
+    if ($CacheType -eq 'All') {
+        Clear-BlackCatCacheInternal -Key $Key -All:$All -CacheType 'MSGraph'
+        Clear-BlackCatCacheInternal -Key $Key -All:$All -CacheType 'AzBatch'
+        Clear-BlackCatCacheInternal -Key $Key -All:$All -CacheType 'General'
+    }
+    else {
+        Clear-BlackCatCacheInternal -Key $Key -All:$All -CacheType $CacheType
+    }
     <#
     .SYNOPSIS
         Clears the cache used by BlackCat functions.
@@ -43,112 +103,4 @@ function Clear-BlackCatCache {
         Use Get-BlackCatCacheStats to see current cache statistics before clearing.
         Tab completion is available for the -Key parameter to show available cache keys.
     #>
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $false)]
-        [ArgumentCompleter({
-            param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
-            
-            # Get the CacheType parameter value from the command
-            $cacheType = $fakeBoundParameters['CacheType']
-            if (-not $cacheType) {
-                $cacheType = 'All'
-            }
-            
-            try {
-                # Get available cache keys for autocompletion by leveraging Get-BlackCatCacheStats
-                $availableKeys = @()
-                $cacheTypes = if ($cacheType -eq 'All') { @('MSGraph', 'AzBatch') } else { @($cacheType) }
-                
-                foreach ($type in $cacheTypes) {
-                    try {
-                        # Use Get-BlackCatCacheStats with ShowDetails to get cache entries with keys
-                        $stats = Get-BlackCatCacheStats -CacheType $type -ShowDetails -Quiet
-                        
-                        if ($stats -and $stats.Entries -and $stats.Entries.Count -gt 0) {
-                            $availableKeys += $stats.Entries | ForEach-Object { $_.Key }
-                        }
-                    }
-                    catch {
-                        # Silently continue if cache access fails
-                        continue
-                    }
-                }
-                
-                # Filter keys based on what user has typed and return them
-                $filteredKeys = $availableKeys | Where-Object { $_ -like "$wordToComplete*" } | Sort-Object
-                
-                # Quote the key if it contains special characters
-                $filteredKeys | ForEach-Object {
-                    if ($_ -match '[\s\|]') {
-                        "'$_'"
-                    } else {
-                        $_
-                    }
-                }
-            }
-            catch {
-                # If anything fails, return empty array
-                @()
-            }
-        })]
-        [string]$Key,
-        
-        [Parameter(Mandatory = $false)]
-        [switch]$All,
-
-        [Parameter(Mandatory = $false)]
-        [ValidateSet('MSGraph', 'AzBatch', 'All')]
-        [string]$CacheType = 'All'
-    )
-
-    if ($CacheType -eq 'All') {
-        # Clear all cache types
-        Clear-BlackCatCacheInternal -Key $Key -All:$All -CacheType 'MSGraph'
-        Clear-BlackCatCacheInternal -Key $Key -All:$All -CacheType 'AzBatch'
-    }
-    else {
-        Clear-BlackCatCacheInternal -Key $Key -All:$All -CacheType $CacheType
-    }
-}
-
-function Get-BlackCatCacheKeys {
-    <#
-    .SYNOPSIS
-        Helper function to get available cache keys for autocompletion.
-    
-    .DESCRIPTION
-        This function retrieves all available cache keys from the specified cache type(s)
-        for use in parameter autocompletion.
-    
-    .PARAMETER CacheType
-        Specifies which cache type to get keys from. Valid values are 'MSGraph', 'AzBatch', or 'All'.
-        Default is 'All' which gets keys from all cache types.
-    #>
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $false)]
-        [ValidateSet('MSGraph', 'AzBatch', 'All')]
-        [string]$CacheType = 'All'
-    )
-    
-    $allKeys = @()
-    $cacheTypes = if ($CacheType -eq 'All') { @('MSGraph', 'AzBatch') } else { @($CacheType) }
-    
-    foreach ($type in $cacheTypes) {
-        try {
-            # Use Get-BlackCatCacheStats with ShowDetails to get cache entries with keys
-            $stats = Get-BlackCatCacheStats -CacheType $type -ShowDetails -Quiet
-            
-            if ($stats -and $stats.Entries -and $stats.Entries.Count -gt 0) {
-                $allKeys += $stats.Entries | ForEach-Object { $_.Key }
-            }
-        }
-        catch {
-            # Silently continue if cache access fails
-            continue
-        }
-    }
-    
-    return $allKeys | Sort-Object
 }
